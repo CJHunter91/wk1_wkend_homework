6be8b64027fefccfdaefe8de196aa805718e0452
Chris Hunter <orkem2@gmail.com>
2017-06-30 14:42:36 +0100

    Added sell_pet_to_customer() final test passed

--- pet_shop.rb
+++ pet_shop.rb
@@ -59,7 +59,5 @@ def sell_pet_to_customer(pet_shop, pet, customer)
     add_or_remove_cash(pet_shop, pet[:price])
     add_pet_to_customer(customer, pet)
     increase_pets_sold(pet_shop, 1)
-  else 
-    nil
   end
 end
